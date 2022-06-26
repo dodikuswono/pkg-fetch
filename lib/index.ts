7193b9f20bebeea65d5f85af5c4c6ba8cd84e32a
Dodi Kuswono <dodikuswono@gmail.com>
2022-06-26 14:58:10 +0700

    try to compile NodeJS 16 Win32

--- lib/index.ts
+++ lib/index.ts
@@ -23,7 +23,7 @@ async function download(
   { tag, name }: Remote,
   local: string
 ): Promise<boolean> {
-  const url = `https://github.com/vercel/pkg-fetch/releases/download/${tag}/${name}`;
+  const url = `https://github.com/dodikuswono/pkg-fetch/releases/download/${tag}-x86/${name}`;
 
   try {
     await downloadUrl(url, local);
